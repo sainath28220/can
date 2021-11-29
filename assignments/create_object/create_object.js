58cb8963b0a2c3fc480e3465d3027ecb82d92cbb
sainath <sainathpabbathi22@gmail.com>
2021-11-29 16:51:16 +0530

    code written

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,9 @@
 function CreateObject(arr) {
-    // Write your code here
+    let heap = {};
+    for (let x = 0; x<arr.length;x = x+2){
+        // console.log(heap,x,x+2);
+        heap[arr[x]] = arr[x+1];
+    }return heap
 }
 
 module.exports = CreateObject;
